d0aea40976755290d63906dd2afe0fc9e3dea5b3
Affax <me@affax.dev>
2020-12-07 13:39:55 +0200

    Fixed deployment

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,9 @@
-const Liquidator = artifacts.require("Liquidator");
+const Layer = artifacts.require("Layer");
 const UniswapDex = artifacts.require("UniswapDex");
 
 module.exports = async function (deployer) {
-	try {
-		await deployer.deploy(Liquidator);
-		await deployer.deploy(UniswapDex);
-	} catch (e) {}
+  try {
+    await deployer.deploy(Layer);
+    await deployer.deploy(UniswapDex);
+  } catch (e) {}
 };
